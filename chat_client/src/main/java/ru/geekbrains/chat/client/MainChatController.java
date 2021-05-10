99704a93e84beb26fe9cd7fcfa03683de781230f
Rustem <79014072+RustemFarkhutdinov@users.noreply.github.com>
2021-05-10 23:22:53 +0300

    HomeWorkLesson4part2

--- chat_client/src/main/java/ru/geekbrains/chat/client/MainChatController.java
+++ chat_client/src/main/java/ru/geekbrains/chat/client/MainChatController.java
@@ -1,17 +1,20 @@
 package ru.geekbrains.chat.client;
 
 
+import javafx.application.HostServices;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-
+import javafx.stage.WindowEvent;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
@@ -23,6 +26,14 @@ public class MainChatController {
     public TextArea chatArea;
     public ListView onlineUsers;
     public Button buttonSendMessage;
+    public Button buttonExit;
+
+    private static HostServices hostServices ;
+
+    public static HostServices getHostServices() {
+        return hostServices ;
+    }
+
 
     public void mockAction(ActionEvent actionEvent) {
         System.out.println("Mockkkk");
@@ -32,14 +43,31 @@ public class MainChatController {
         Platform.exit();
     }
 
+    public void exitWindow(ActionEvent actionEvent) {
+        Stage stage = (Stage) buttonExit.getScene().getWindow();
+        stage.close();
+    }
+
     public void showAbout(ActionEvent actionEvent) throws IOException {
-        App showAbout = FXMLLoader.load(getClass().getResource("/newScene.fxml"));
-        Stage stage = new Stage();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/newScene.fxml"));
+        Parent root = loader.load();
+
+        Stage newStageAbout = new Stage();
+        Scene scene = new Scene(root, 600, 600);
 
 
-        stage.setScene(new Scene()); //Надо было создать в App классе?!
-        stage.setResizable(false);
-        stage.show();
+        newStageAbout.setScene(scene);
+        newStageAbout.setResizable(false);
+        newStageAbout.setTitle("About");
+        newStageAbout.setOnHidden(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                System.out.println("Close 'About' Window!");
+            }
+        });
+        newStageAbout.show();
             }
 
 
@@ -49,6 +77,15 @@ public class MainChatController {
         desktop.browse(new URI("https://vk.com/bezvozmezdnopomoshh"));
 
     }
+
+    //Почему не работает?(((
+    public void showMySite(ActionEvent actionEvent) throws URISyntaxException, IOException {
+        final Hyperlink hyperlink = new Hyperlink("https://gb.ru/");
+        hyperlink.setOnAction(t -> {
+           getHostServices().showDocument(hyperlink.getText());
+        });
+    }
+
 
     public void sendMessage(ActionEvent actionEvent) {
         appendTextFromTF();
